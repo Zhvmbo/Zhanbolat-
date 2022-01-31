e92cebb92cca409891b57a0ad7af69f956f0ba75
Zhvmbo <96414238+Zhvmbo@users.noreply.github.com>
2022-01-31 19:07:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction(){
+  var x = document.getElementById("myText").name;
+  document.getElementById("demo").innerHTML = x;
+}
+function multiply()
+    {
+      num1 = document.getElementById("first").value;
+      num2 = document.getElementById("second").value;
+      document.getElementById("result").innerHTML = num1 * num2;
+    };
+  function divide()
+  {
+    num1 = document.getElementById("first").value;
+    num2 = document.getElementById("second").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+  }
